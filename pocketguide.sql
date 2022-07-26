5f9c74ab4dad35f9bf78e359fe6cfed771f6e14c
unknown <jctmasangkay@donbosco.edu.ph>
2022-07-26 12:05:28 +0800

    Adding the SQL File

---
+++ pocketguide.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2022 at 05:55 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pocketguide`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_announcement`
+--
+
+CREATE TABLE `tbl_announcement` (
+  `a_id` int(100) NOT NULL,
+  `date_posted` varchar(100) NOT NULL,
+  `a_author` varchar(200) NOT NULL,
+  `a_subject` varchar(200) NOT NULL,
+  `a_content` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_announcement`
+--
+
+INSERT INTO `tbl_announcement` (`a_id`, `date_posted`, `a_author`, `a_subject`, `a_content`) VALUES
+(1, '2022-07-26T03:46:09+00:00', '1', 'Typhoon', 'No Class');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_announcement`
+--
+ALTER TABLE `tbl_announcement`
+  ADD PRIMARY KEY (`a_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
