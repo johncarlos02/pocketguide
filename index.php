82dc6f9fde579d4552bffb9107efa100e7b97b41
unknown <jctmasangkay@donbosco.edu.ph>
2022-07-26 11:11:45 +0800

    Initial Commit

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
-   echo "Hello";
+ include('settings.php');
+
+ session_start();
+ if(isset($_SESSION['uid'])){
+   include('home.php');
+
+ }else{
+   include('authentication/login.php');
+ }
 ?>
---
+++ settings.php
@@ -0,0 +1,13 @@
+<?php
+    $database = 'mysql:host=localhost; dbname=pocketguide';
+    $user = 'pocketguide/admin';
+    $pwd = '123';
+
+    try{
+        $pdo = new PDO($database, $user, $pwd);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //echo 'Connection success!';
+    }catch(PDOException $e){
+        echo 'Connection failed: '.$e->getMessage();
+    }
+?>
